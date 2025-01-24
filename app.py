a28250a88e089f532675f672b09b3532ecb89b9b
Lucila Chaparro <lucilachaparro@gmail.com>
2025-01-24 14:34:55 -0300

    modification for prod deployment

--- app.py
+++ app.py
@@ -39,6 +39,3 @@ def ask_question():
         print("Error occurred while processing the query:")
         print(format_exc())
         return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(debug=True)
